e0f788cd960204fd62f49e1aa9cb3360e9eac607
strongleanEva <evii.x33@web.de>
2026-06-26 21:31:28 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,4 @@
-const CACHE_NAME = "strong-lean-v1";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
-});
+const CACHE='strong-lean-v1-1';
+const ASSETS=['./','./index.html','./css/app.css','./js/app.js','./data/week1.js','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
